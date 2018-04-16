16f83498d8d67cd5402e07b17b684cbf0b8d3cdf
Wolf Syperek <wsyperek@gmail.com>
2018-04-16 21:42:41 +0200

    First

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,19 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+// platformBrowserDynamic().bootstrapModule(AppModule)  .catch(err => console.log(err));
+
+
+const bootApplication = () => {
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch(err => console.log(err));
+};
+
+if (document.readyState === 'complete') {
+  window.onload = () => bootApplication();
+  location.reload();
+}
+else {
+  document.addEventListener('DOMContentLoaded', bootApplication);
+}
